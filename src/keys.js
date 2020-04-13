c508d6bbdfacc3a4b86c87b86f334dc31062849c
Gaston Muñoz <cgmtandil@gmail.com>
2020-04-13 08:41:21 -0300

    Añadiendo Mongo Atlas

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,6 @@
 module.exports = {
-
-database : {
-    URI: 'mongodb://localhost/img-social'
-
-}
-
-
+  database: {
+    URI:
+      "mongodb+srv://gaston:victoriatomas28@cluster0-hzgux.mongodb.net/img-social?retryWrites=true&w=majority",
+  },
 };
